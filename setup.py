14b4ffb9a371c1d050190ce131832a7378b70ba3
wrongu <richard.d.lange.13@dartmouth.edu>
2022-12-21 15:47:52 -0500

    move from setup.py to setup.cfg

--- setup.py
+++ setup.py
@@ -1,34 +1,5 @@
 #!/usr/bin/env python3
+from setuptools import setup
 
-import os
-import io
-from setuptools import find_packages, setup
-
-"""
-git tag {VERSION}
-git push --tags
-python setup.py sdist
-twine upload dist/*
-"""
-
-VERSION = "0.7.8"
-
-here = os.path.abspath(os.path.dirname(__file__))
-with io.open(os.path.join(here, "README.md"), encoding="utf-8") as f:
-    long_description = "\n" + f.read()
-
-setup(
-    name="repsim",
-    version=VERSION,
-    author="Richard Lange",
-    description=("Representational similarity and geometry"),
-    long_description=long_description,
-    long_description_content_type="text/markdown",
-    license="MIT",
-    url="https://github.com/wrongu/repsim/tarball/" + VERSION,
-    packages=find_packages(exclude=["tests", "*.tests", "*.tests.*", "tests.*"]),
-    classifiers=[],
-    install_requires=[
-        "torch"
-    ]
-)
+if __name__ == "__main__":
+    setup()
