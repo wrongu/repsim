c3d30ecafa8cb56f7e1c22c89fabb963b148a5a5
wrongu <richard.d.lange.13@dartmouth.edu>
2023-02-23 16:31:02 -0500

    refactored test_names to use pytest fixtures

--- tests/metrics.py
+++ tests/metrics.py
@@ -11,6 +11,7 @@ from tests.constants import size_m, size_n
 _list_of_metrics = [
     # Angular CKA with a linear kernel is not affine invariant
     {"metric": AngularCKA(size_m),
+     "name": f"AngularCKA.Linear.{size_m}",
      "expected-curvature": "positive",
      "high-rank-data": False,
      "rotation-invariant": True,
@@ -19,6 +20,7 @@ _list_of_metrics = [
      "affine-invariant": False},
     # Next two tests: assert that scale invariance is due to adaptive length scale in the kernel when using sqexp
     {"metric": AngularCKA(size_m, kernel=SquaredExponential(length_scale='median')),
+     "name": f"AngularCKA.SqExp[median].{size_m}",
      "expected-curvature": "positive",
      "high-rank-data": False,
      "rotation-invariant": True,
@@ -26,6 +28,7 @@ _list_of_metrics = [
      "scale-invariant": True,
      "affine-invariant": False},
     {"metric": AngularCKA(size_m, kernel=SquaredExponential(length_scale=0.5)),
+     "name": f"AngularCKA.SqExp[0.500].{size_m}",
      "expected-curvature": "positive",
      "high-rank-data": False,
      "rotation-invariant": True,
@@ -34,6 +37,7 @@ _list_of_metrics = [
      "affine-invariant": False},
     # Next two tests: Stress with a linear kernel is scale-invariant only if rescale=True is set
     {"metric": Stress(size_m, kernel=Linear()),
+     "name": f"Stress.Linear.{size_m}",
      "expected-curvature": "zero",
      "high-rank-data": False,
      "rotation-invariant": True,
@@ -41,6 +45,7 @@ _list_of_metrics = [
      "scale-invariant": False,
      "affine-invariant": False},
     {"metric": Stress(size_m, kernel=Linear(), rescale=True),
+     "name": f"Stress.Linear.scaled.{size_m}",
      "expected-curvature": "zero",
      "high-rank-data": False,
      "rotation-invariant": True,
@@ -49,6 +54,7 @@ _list_of_metrics = [
      "affine-invariant": False},
     # Next two tests: Stress with an auto-length-scale kernel is scale invariant
     {"metric": Stress(size_m, kernel=SquaredExponential()),
+     "name": f"Stress.SqExp[auto].{size_m}",
      "expected-curvature": "zero",
      "high-rank-data": False,
      "rotation-invariant": True,
@@ -56,6 +62,7 @@ _list_of_metrics = [
      "scale-invariant": True,
      "affine-invariant": False},
     {"metric": Stress(size_m, kernel=Laplace()),
+     "name": f"Stress.Laplace[auto].{size_m}",
      "expected-curvature": "zero",
      "high-rank-data": False,
      "rotation-invariant": True,
@@ -64,6 +71,7 @@ _list_of_metrics = [
      "affine-invariant": False},
     # The 'affine invariant' metric is not, in fact, affine-invariant when using a kernel
     {"metric": AffineInvariantRiemannian(m=size_m, kernel=SquaredExponential()),
+     "name": f"AffineInvariantRiemannian[gram].SqExp[auto].{size_m}",
      "expected-curvature": "negative",
      "high-rank-data": False,
      "rotation-invariant": True,
@@ -71,6 +79,7 @@ _list_of_metrics = [
      "scale-invariant": True,
      "affine-invariant": False},
     {"metric": AffineInvariantRiemannian(m=size_m, kernel=Laplace()),
+     "name": f"AffineInvariantRiemannian[gram].Laplace[auto].{size_m}",
      "expected-curvature": "negative",
      "high-rank-data": False,
      "rotation-invariant": True,
@@ -79,14 +88,16 @@ _list_of_metrics = [
      "affine-invariant": False},
     # The 'affine invariant' metric *is* affine-invariant using a linear kernel, but only works if n > size_m
     {"metric": AffineInvariantRiemannian(m=size_m, kernel=Linear()),
+     "name": f"AffineInvariantRiemannian[gram].Linear.{size_m}",
      "expected-curvature": "negative",
-     "high-rank-data": True,
+     "high-rank-data": True,  # (!!) AffineInvariantRiemannian with a Linear kernel requires n > m
      "rotation-invariant": True,
      "translation-invariant": True,
      "scale-invariant": True,
      "affine-invariant": True},
     # ...If we instead use a Linear kernel with eps regularization, we lose the scale and affine invariance again
     {"metric": AffineInvariantRiemannian(m=size_m, kernel=Linear(), eps=0.01),
+     "name": f"AffineInvariantRiemannian[gram][0.010].Linear.{size_m}",
      "expected-curvature": "negative",
      "high-rank-data": True,
      "rotation-invariant": True,
@@ -95,6 +106,7 @@ _list_of_metrics = [
      "affine-invariant": False},
     # Shape metrics with alpha=0 (full whitening) are invariant to everything...
     {"metric": EuclideanShapeMetric(m=size_m, p=size_n, alpha=0.0),
+     "name": f"ShapeMetric[0.00][{size_n}][euclidean].{size_m}",
      "expected-curvature": "positive",  # (!!) EuclideanShapeMetric curvature ≥ 0 when alpha < 1
      "high-rank-data": False,
      "rotation-invariant": True,
@@ -103,6 +115,7 @@ _list_of_metrics = [
      "affine-invariant": True},
     # ...unless p<n, in which case they will in general lose affine invariance because top-p PCs changed
     {"metric": EuclideanShapeMetric(m=size_m, p=size_n//2, alpha=0.0),
+     "name": f"ShapeMetric[0.00][{size_n//2}][euclidean].{size_m}",
      "expected-curvature": "positive",  # (!!) EuclideanShapeMetric curvature ≥ 0 when alpha < 1
      "high-rank-data": False,
      "rotation-invariant": True,
@@ -111,6 +124,7 @@ _list_of_metrics = [
      "affine-invariant": False},
     # Shape metrics are also not affine-invariant when doing partial (alpha=0.5) or no (alpha=1.0) whitening
     {"metric": EuclideanShapeMetric(m=size_m, p=size_n, alpha=0.5),
+     "name": f"ShapeMetric[0.50][{size_n}][euclidean].{size_m}",
      "expected-curvature": "positive",  # (!!) EuclideanShapeMetric curvature ≥ 0 when alpha < 1
      "high-rank-data": False,
      "rotation-invariant": True,
@@ -118,6 +132,7 @@ _list_of_metrics = [
      "scale-invariant": True,
      "affine-invariant": False},
     {"metric": EuclideanShapeMetric(m=size_m, p=size_n, alpha=1.0),
+     "name": f"ShapeMetric[1.00][{size_n}][euclidean].{size_m}",
      "expected-curvature": "zero",
      "high-rank-data": False,
      "rotation-invariant": True,
@@ -126,6 +141,7 @@ _list_of_metrics = [
      "affine-invariant": False},
     # ...repeat the last 4 tests for the AngularShapeMetric
     {"metric": AngularShapeMetric(m=size_m, p=size_n, alpha=0.0),
+     "name": f"ShapeMetric[0.00][{size_n}][angular].{size_m}",
      "expected-curvature": "positive",
      "high-rank-data": False,
      "rotation-invariant": True,
@@ -133,6 +149,7 @@ _list_of_metrics = [
      "scale-invariant": True,
      "affine-invariant": True},
     {"metric": AngularShapeMetric(m=size_m, p=size_n//2, alpha=0.0),
+     "name": f"ShapeMetric[0.00][{size_n//2}][angular].{size_m}",
      "expected-curvature": "positive",
      "high-rank-data": False,
      "rotation-invariant": True,
@@ -140,6 +157,7 @@ _list_of_metrics = [
      "scale-invariant": True,
      "affine-invariant": False},
     {"metric": AngularShapeMetric(m=size_m, p=size_n, alpha=0.5),
+     "name": f"ShapeMetric[0.50][{size_n}][angular].{size_m}",
      "expected-curvature": "positive",
      "high-rank-data": False,
      "rotation-invariant": True,
@@ -147,6 +165,7 @@ _list_of_metrics = [
      "scale-invariant": True,
      "affine-invariant": False},
     {"metric": AngularShapeMetric(m=size_m, p=size_n, alpha=1.0),
+     "name": f"ShapeMetric[1.00][{size_n}][angular].{size_m}",
      "expected-curvature": "positive",
      "high-rank-data": False,
      "rotation-invariant": True,
--- tests/test_names.py
+++ tests/test_names.py
@@ -2,52 +2,6 @@ from repsim import AngularCKA, AffineInvariantRiemannian, Stress, AngularShapeMe
 from repsim.kernels import SquaredExponential
 
 
-def test_names():
-    # BASICS - DEFAULT KERNELS
-    metric = AngularCKA(m=1000)
-    assert metric.string_id() == "AngularCKA.Linear.1000"
-
-    metric = AffineInvariantRiemannian(m=1000)
-    assert metric.string_id() == "AffineInvariantRiemannian[gram].Linear.1000"
-
-    metric = AffineInvariantRiemannian(m=1000, eps=0.05)
-    assert metric.string_id() == "AffineInvariantRiemannian[gram][0.050].Linear.1000"
-
-    metric = AffineInvariantRiemannian(m=1000, eps=0.05, mode="cov", p=100)
-    assert metric.string_id() == "AffineInvariantRiemannian[cov][0.050][100].1000"
-
-    metric = Stress(m=1000)
-    assert metric.string_id() == "Stress.Linear.1000"
-
-    # KERNEL WITH AUTO SCALE
-    metric = AngularCKA(m=1000, kernel=SquaredExponential())
-    assert metric.string_id() == "AngularCKA.SqExp[auto].1000"
-
-    metric = AffineInvariantRiemannian(m=1000, kernel=SquaredExponential())
-    assert metric.string_id() == "AffineInvariantRiemannian[gram].SqExp[auto].1000"
-
-    metric = Stress(m=1000, kernel=SquaredExponential())
-    assert metric.string_id() == "Stress.SqExp[auto].1000"
-
-    # KERNEL WITH LENGTH SCALE
-    metric = AngularCKA(m=100, kernel=SquaredExponential(length_scale=0.3))
-    assert metric.string_id() == "AngularCKA.SqExp[0.300].100"
-
-    metric = AffineInvariantRiemannian(m=100, kernel=SquaredExponential(length_scale=0.3))
-    assert metric.string_id() == "AffineInvariantRiemannian[gram].SqExp[0.300].100"
-
-    metric = Stress(m=100, kernel=SquaredExponential(length_scale=0.3))
-    assert metric.string_id() == "Stress.SqExp[0.300].100"
-
-    # SHAPE METRICS
-    metric = AngularShapeMetric(m=1000, p=100)
-    assert metric.string_id() == "ShapeMetric[1.00][100][angular].1000"
-
-    metric = EuclideanShapeMetric(m=1000, p=100)
-    assert metric.string_id() == "ShapeMetric[1.00][100][euclidean].1000"
-
-    metric = AngularShapeMetric(m=100, p=40, alpha=0.5)
-    assert metric.string_id() == "ShapeMetric[0.50][40][angular].100"
-
-    metric = EuclideanShapeMetric(m=100, p=40, alpha=0.5)
-    assert metric.string_id() == "ShapeMetric[0.50][40][euclidean].100"
+def test_name(metric):
+    # Note: the expected value of each test_name is defined in tests/metrics.py
+    assert metric.string_id() == metric.test_name
